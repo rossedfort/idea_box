3736e889c87106526e5306e1c181300f07188701
rossedfort <rossedfort@gmail.com>
2015-11-05 14:57:24 -0700

    implement user login

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -8,13 +8,37 @@ feature "Signing in" do
               password: 'password')
   end
 
-  pending"Signing in with correct credentials" do
-    visit '/sessions/new'
+  scenario "Signing in with correct credentials" do
+    visit '/login'
     within("#session") do
       fill_in 'Username', :with => 'rossedfort'
       fill_in 'Password', :with => 'password'
     end
     click_button 'Sign in'
     expect(page).to have_content 'Welcome Ross'
+  end
+
+  scenario "Signing in with incorrect username" do
+    visit '/login'
+    within("#session") do
+      fill_in 'Username', :with => 'notrossedfort'
+      fill_in 'Password', :with => 'password'
+    end
+    click_button 'Sign in'
+    within("#errors") do
+      expect(page).to have_content 'Incorrect Credentials'
+    end
+  end
+
+  scenario "Signing in with incorrect password" do
+    visit '/login'
+    within("#session") do
+      fill_in 'Username', :with => 'rossedfort'
+      fill_in 'Password', :with => 'notapassword'
+    end
+    click_button 'Sign in'
+    within("#errors") do
+      expect(page).to have_content 'Incorrect Credentials'
+    end
   end
 end
