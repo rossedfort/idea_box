1fb8b7466567d69c129c51ed2a46cb9fae274615
Emily Dowdle <emily@editingemily.com>
2015-11-05 19:30:46 -0700

    Implement admin authorization
    Includes:
    - Namespace of :admin to contain :categories routes
    - Enum for User role, defaults to default, may be set as admin
    - Limits users from seeing admin pages by directing to 404 page

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'capybara'
   gem 'launchy'
   gem 'byebug'
+  gem 'rspec-mocks'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  namespace :admin do
+    resources :categories
+  end
 end
