1f295e336242a9d7e37212956c766075bd6089ae
rossedfort <rossedfort@gmail.com>
2015-11-05 21:10:42 -0700

    add test for sad path user creation

--- spec/features/guest_can_create_acct_spec.rb
+++ spec/features/guest_can_create_acct_spec.rb
@@ -18,4 +18,17 @@ feature "Signing up" do
 
     expect(page).to have_content 'Welcome Ross'
   end
+
+  scenario "Signing up with incorrect creditionals" do
+    visit '/users/new'
+    within("#signup-form") do
+      fill_in 'First name', :with => 'Ross'
+      fill_in 'Username', :with => 'rossedfort'
+      fill_in 'Password', :with => 'password'
+    end
+
+    click_button 'Sign up'
+
+    expect(page).to_not have_content 'Welcome Ross'
+  end
 end
