14bd1f834873bc6010bef7c27cab81a4aca4b02f
Emily Dowdle <emily@editingemily.com>
2015-11-05 19:33:39 -0700

    Minor refactor

--- spec/features/admin_can_create_category_spec.rb
+++ spec/features/admin_can_create_category_spec.rb
@@ -23,8 +23,7 @@ feature "Signing in" do
                 password: 'password',
                 role: 1)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)    
-    # ApplicationController.any_instance.stubs(:current_user).returns(admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
     visit admin_categories_path
 
     assert page.has_content?("All Categories")
