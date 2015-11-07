ba1bf6b53f2445fcfbec2bf2b3cc1b3e10d0f6b3
rossedfort <rossedfort@gmail.com>
2015-11-07 14:24:47 -0700

    took out faker seeds - heroku was complaining

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,6 @@
 class Seed
   def initialize
     generate_users
-    generate_categories
-    generate_images
-    generate_ideas
   end
 
   def generate_users
@@ -22,48 +19,6 @@ class Seed
                      role: 1)
 
     puts "#{me.username} - #{me.password} ADMIN created!"
-
-    4.times do |i|
-      user = User.create(first_name: Faker::Name.first_name,
-                         last_name: Faker::Name.last_name,
-                         username: Faker::Internet.user_name,
-                         password: Faker::Internet.password,
-                         role: 0
-                        )
-      puts "#{user.username} - #{user.password} DEFAULT created!"
-    end
-  end
-
-  def generate_ideas
-    50.times do |_i|
-      idea = Idea.create(name: Faker::Lorem.word,
-                         description: Faker::Lorem.sentence,
-                         user_id: rand(1..5),
-                         category_id: rand(1..10),
-                         image_id: rand(1..10)
-                        )
-
-      puts "#{idea.name} created"
-    end
-  end
-
-  def generate_categories
-    10.times do |i|
-      category = Admin::Category.create(name: Faker::Lorem.word,
-                                        description: Faker::Lorem.sentence,
-                                        user_id: 1)
-
-      puts "#{category.name} category created!"
-    end
-  end
-
-  def generate_images
-    10.times do |i|
-      image = Admin::Image.create(name: Faker::Lorem.word,
-                                  url: Faker::Avatar.image,
-                                  user_id: 1,
-                                  )
-    end
   end
 end
 
