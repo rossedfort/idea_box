da16ae535668f8e49afe267c1286ede7fe464ebb
rossedfort <rossedfort@gmail.com>
2015-11-06 19:29:05 -0700

    fix merge conflict

--- app/models/admin/category.rb
+++ app/models/admin/category.rb
@@ -1,11 +1,5 @@
-<<<<<<< HEAD:app/models/admin/category.rb
 class Admin::Category < ActiveRecord::Base
-||||||| merged common ancestors
-class Category < ActiveRecord::Base
-=======
-class Category < ActiveRecord::Base
   has_many :ideas
->>>>>>> user-flow:app/models/category.rb
   belongs_to :user
 
   validates :name, presence: true,
